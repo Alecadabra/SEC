0950e82bcdb8d65671bf48f41d5cc51843cff7e0
Alec PC <27219575+Alecadabra@users.noreply.github.com>
2021-09-15 19:35:52 +0800

    Attempt at fixing freeze

--- Assignment 1/src/main/kotlin/alec/sec/assignment1/FileSearcher.kt
+++ Assignment 1/src/main/kotlin/alec/sec/assignment1/FileSearcher.kt
@@ -22,7 +22,7 @@ class FileSearcher(
             .filter { file ->
                 setOf(".txt", ".md", ".java", ".kt", ".cs").any { ext -> file.name.endsWith(ext) }
             }
-            .filter { file -> file.isNotEmpty() }
+            .filter { file -> file.length() != 0L } // TODO
             .flowOn(Dispatchers.IO)
 
     // TODO Implement cancel functionality
@@ -31,25 +31,33 @@ class FileSearcher(
             // Don't compare a file to itself, and only compare each file once
             .filter { file2 -> file1.absolutePath > file2.absolutePath }
             .flowOn(Dispatchers.Default)
-            .map { file2 ->
-                ReadFile(file1, file1.readText()) to ReadFile(file2, file2.readText())
-            }
+//            .map { file2 ->
+//                ReadFile(file1, file1.readText()) to ReadFile(file2, file2.readText())
+//            } // TODO
             .flowOn(Dispatchers.IO)
-            .map { (readFile1, readFile2) ->
+//            .map { (readFile1, readFile2) ->
+//                ComparisonResult(
+//                    readFile1.file.name,
+//                    readFile2.file.name,
+//                    calcSimilarity(readFile1.string, readFile2.string),
+//                )
+//            }TODO
+            .map { file2 ->
                 ComparisonResult(
-                    readFile1.file.name,
-                    readFile2.file.name,
-                    calcSimilarity(readFile1.string, readFile2.string),
+//TODO Remove
+                    file1.name,
+                    file2.name,
+                    calcSimilarity(file1.readText(), file2.readText()),
                 )
             }
             .flowOn(Dispatchers.Default)
-            .onEach { result -> this.resultsWriter.writeResult(result) }
+//            .onEach { result -> this.resultsWriter.writeResult(result) }TODO
             .flowOn(Dispatchers.IO)
             .onEach { this.ui.progress++ }
             .flowOn(Dispatchers.Main)
     }
         .filter { result -> result.similarity >= similarityThreshold }
-        .onEach { result -> this.ui.addComparisonResult(result) }
+//        .onEach { result -> this.ui.addComparisonResult(result) }
         .catch { e -> println("An error happened: ${e.message}") }
         .flowOn(Dispatchers.Main)
 
@@ -57,14 +65,24 @@ class FileSearcher(
         // The number of comparisons is half size of the n * n comparison matrix, with the leading
         // diagonal removed, where n is the number of files after filtering.
         val numComparisons = withContext(Dispatchers.IO) {
-             this@FileSearcher.fileFlow.count().let { (it * it - it) / 2 }
+            this@FileSearcher.fileFlow.count().let { (it * it - it) / 2 }
         }
 
         // Set the total value for the UI progress bar
         withContext(Dispatchers.Main) { this@FileSearcher.ui.progressTotal = numComparisons }
 
         // Run and terminate the flow
-        return this.fileSearchFlow.launchIn(scope)
+//        return scope.launch {
+//            this@FileSearcher.fileSearchFlow.collect { result ->
+//                this@FileSearcher.ui.addComparisonResult(result)
+//            }
+//        }
+        this@FileSearcher.fileSearchFlow.collect { result ->
+            withContext(Dispatchers.Main) {
+                this@FileSearcher.ui.addComparisonResult(result)
+            }
+        }
+        return Job()
     }
 }
 
