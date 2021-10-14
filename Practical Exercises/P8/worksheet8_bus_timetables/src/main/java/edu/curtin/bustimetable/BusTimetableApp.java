867f1f1d2e114fff06882f7910b58111708bb451
Alecadabra [Laptop] <27219575+Alecadabra@users.noreply.github.com>
2021-10-14 14:15:22 +0800

    Better code for locale parsing

--- Practical Exercises/P8/worksheet8_bus_timetables/src/main/java/edu/curtin/bustimetable/BusTimetableApp.java
+++ Practical Exercises/P8/worksheet8_bus_timetables/src/main/java/edu/curtin/bustimetable/BusTimetableApp.java
@@ -23,23 +23,59 @@ public class BusTimetableApp extends Application
     public void start(Stage stage)
     {
         var localeString = getParameters().getNamed().get("locale");
-        Locale locale;
-        if(localeString == null)
-        {
-            locale = Locale.getDefault();
-        }
-        else
-        {
-            locale = Locale.forLanguageTag(localeString);
-        }
-        System.out.println("Localestring: " + localeString + ", locale: " + locale);
-
-        ResourceBundle strings = ResourceBundle.getBundle("strings", locale);
+        ResourceBundle strings = getStrings(localeString);
     
         var entries = FXCollections.<TimetableEntry>observableArrayList();
         FileIO fileIO = new FileIO(strings);
         LoadSaveUI loadSaveUI = new LoadSaveUI(stage, entries, fileIO, strings);
         AddUI addUI = new AddUI(entries, strings);
         new MainUI(stage, entries, loadSaveUI, addUI, strings).display();
+    }
+
+    /**
+     * Gets the resource bundle for translations, from the possibly-null
+     * command line parameter --locale=[value].
+     * Prints out messages about the parsed value.
+     */
+    private ResourceBundle getStrings(String localeString)
+    {
+        ResourceBundle strings;
+        if(localeString == null)
+        {
+            Locale locale = Locale.getDefault();
+            strings = ResourceBundle.getBundle("strings", locale);
+
+            if (strings.getLocale().equals(Locale.getDefault()))
+            {
+                System.out.println("Loading locale '" + strings.getLocale() + "' (default)");
+            }
+            else
+            {
+                System.out.println("Loading locale '" + strings.getLocale() + "' (no match for default locale '" + locale + "')");
+            }
+        }
+        else
+        {
+            Locale locale = Locale.forLanguageTag(localeString);
+            strings = ResourceBundle.getBundle("strings", locale);
+
+            if (strings.getLocale().equals(Locale.getDefault()))
+            {
+                if (strings.getLocale().equals(locale))
+                {
+                    System.out.println("Loading locale '" + strings.getLocale() + "' (default)");
+                }
+                else
+                {
+                    System.out.println("Loading locale '" + strings.getLocale() + "' (default) (no match for command-line input '" + localeString + "')");
+                }
+            }
+            else
+            {
+                System.out.println("Loading locale '" + strings.getLocale() + "' from command-line input");
+            }
+        }
+
+        return strings;
     }
 }
